32fa37d6f7c035ff3db15c6bf21a4055635a0b90
ElisKina-dev <elis.kina@nttdata.com>
2024-12-06 17:38:10 +0100

    fix path

--- src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
-@RepositoryRestResource(path = "organization", collectionResourceRel = "organization")
+@RepositoryRestResource(path = "organizations", collectionResourceRel = "organization")
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
 
   Optional<Organization> findByIpaCode(String ipaCode);
