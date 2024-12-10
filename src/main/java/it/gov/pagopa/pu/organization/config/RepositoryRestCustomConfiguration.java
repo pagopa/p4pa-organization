80bd34827dc47ddad38f7182a9e253630d52baaf
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2024-12-10 12:24:49 +0100

    added unit test

--- src/main/java/it/gov/pagopa/pu/organization/config/RepositoryRestCustomConfiguration.java
+++ src/main/java/it/gov/pagopa/pu/organization/config/RepositoryRestCustomConfiguration.java
@@ -3,10 +3,8 @@ package it.gov.pagopa.pu.organization.config;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import java.util.Set;
-import java.util.function.Consumer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @Configuration
@@ -18,16 +16,12 @@ public class RepositoryRestCustomConfiguration {
   }
   @Bean
   public RepositoryRestConfigurer repositoryRestConfigurer() {
-    return RepositoryRestConfigurer.withConfig( config() );
-  }
-
-  Consumer<RepositoryRestConfiguration> config () {
-    return config -> {
+    return RepositoryRestConfigurer.withConfig( config -> {
       Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
       config.exposeIdsFor(entities.stream()
         .map(EntityType::getJavaType)
         .toArray(Class[]::new));
-    };
+    } );
   }
 
 }
--- src/test/java/it/gov/pagopa/pu/organization/config/RepositoryRestCustomConfigurationTest.java
+++ src/test/java/it/gov/pagopa/pu/organization/config/RepositoryRestCustomConfigurationTest.java
@@ -7,8 +7,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import jakarta.persistence.metamodel.Metamodel;
 import java.util.Collections;
-import java.util.function.Consumer;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,27 +33,22 @@ class RepositoryRestCustomConfigurationTest {
   @InjectMocks
   private RepositoryRestCustomConfiguration config;
 
-  @Test
-  void givenRepositoryRestConfigurerThenOk() {
-    // Act
-    RepositoryRestConfigurer configurer = config.repositoryRestConfigurer();
-
-    // Assert
-    Assertions.assertNotNull(configurer);
-  }
-
-  @Test
-  void givenCustomRepositoryRestConfigurationConfigurationThenOk() {
+  @BeforeEach
+  void setUp(){
     Mockito.when(entityManager.getMetamodel()).thenReturn(metamodel);
     Mockito.when(metamodel.getEntities()).thenReturn(Collections.singleton(entityType));
     Mockito.when(entityType.getJavaType()).thenReturn((Class) Broker.class);
+  }
+
+  @Test
+  void givenRepositoryRestConfigurerThenOk() {
+    RepositoryRestConfiguration repositoryRestConfiguration = Mockito.mock(RepositoryRestConfiguration.class);
     // Act
-    Consumer<RepositoryRestConfiguration> consumer = config.config();
+    RepositoryRestConfigurer configurer = config.repositoryRestConfigurer();
+    configurer.configureRepositoryRestConfiguration(repositoryRestConfiguration,null);
 
     // Assert
-    Assertions.assertNotNull(consumer);
-    RepositoryRestConfiguration repositoryRestConfiguration = Mockito.mock(RepositoryRestConfiguration.class);
-    consumer.accept(repositoryRestConfiguration);
+    Assertions.assertNotNull(configurer);
     Mockito.verify(entityManager, times(1)).getMetamodel();
     Mockito.verify(metamodel, times(1)).getEntities();
     Mockito.verify(entityType, times(1)).getJavaType();
