e96d51f8751d3bc09728161be376ad086f25ecad
ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
2024-12-18 12:00:25 +0100

    feat: GET organization broker by fiscal code (#7)

--- src/main/java/it/gov/pagopa/pu/organization/repository/BrokerRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/BrokerRepository.java
@@ -2,9 +2,16 @@ package it.gov.pagopa.pu.organization.repository;
 
 import it.gov.pagopa.pu.organization.model.Broker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "broker", path = "brokers")
 public interface BrokerRepository extends JpaRepository<Broker,Long> {
+
+  @Query("select b from broker b join Organization o on b.brokerId = o.brokerId " +
+    "where o.orgFiscalCode = :orgFiscalCode")
+  Optional<Broker> findByOrgFiscalCode(String orgFiscalCode);
 
 }
