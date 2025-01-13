e39f2d587ee67d386c72a066b1e011597d220aaa
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2025-01-13 18:00:08 +0100

    updated code

--- src/test/java/it/gov/pagopa/pu/organization/controller/TaxonomyControllerTest.java
+++ src/test/java/it/gov/pagopa/pu/organization/controller/TaxonomyControllerTest.java
@@ -9,7 +9,6 @@ import it.gov.pagopa.pu.organization.service.TaxonomyService;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,7 +16,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,32 +25,32 @@ import org.springframework.test.web.servlet.MockMvc;
 })
 @AutoConfigureMockMvc(addFilters = false)
 class TaxonomyControllerTest {
+
   @Autowired
   private MockMvc mockMvc;
+
   @MockitoBean
   private TaxonomyService taxonomyServiceMock;
-  @InjectMocks
-  private TaxonomyController taxonomyController;
 
   @Test
-  void getDistinctOrgType () throws Exception {
-    List<DistinctOrganizationTypeDTO> orgTypes = new ArrayList<DistinctOrganizationTypeDTO>();
-    orgTypes.add(0, new DistinctOrganizationTypeDTO() {
+  void getDistinctOrgType() throws Exception {
+    List<DistinctOrganizationTypeDTO> orgTypes = new ArrayList<>();
+    orgTypes.add(new DistinctOrganizationTypeDTO() {
       @Override
-      public String getOrganizationType () {
+      public String getOrganizationType() {
         return "test";
       }
 
       @Override
-      public String getOrganizationTypeDescription () {
+      public String getOrganizationTypeDescription() {
         return "test";
       }
     });
 
-    Mockito.when(taxonomyServiceMock.getDistinctOrganizationType(Sort.by(Order.asc("organizationType")))).thenReturn(orgTypes);
+    Mockito.when(taxonomyServiceMock.getDistinctOrganizationType(Sort.by(Sort.Order.asc("organizationType")))).thenReturn(orgTypes);
 
     mockMvc.perform(get("/taxonomy/custom/getDistinctOrganizationType")
-        .param("sort","organizationType,ASC")
+        .param("sort", "organizationType,ASC")
         .accept(MediaType.APPLICATION_JSON))
       .andExpect(status().isOk())
       .andExpect(content().contentType(MediaType.APPLICATION_JSON))
