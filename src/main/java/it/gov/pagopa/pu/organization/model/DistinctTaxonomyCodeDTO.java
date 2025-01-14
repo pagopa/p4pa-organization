06d4a12ec0d1fe33885323b3311f0051c0d93a37
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2025-01-14 11:59:08 +0100

    Update src/main/java/it/gov/pagopa/pu/organization/model/DistinctTaxonomyCodeDTO.java
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/pu/organization/model/DistinctTaxonomyCodeDTO.java
+++ src/main/java/it/gov/pagopa/pu/organization/model/DistinctTaxonomyCodeDTO.java
@@ -10,7 +10,7 @@ import lombok.Data;
 @AllArgsConstructor
 @Entity
 @Table(name = "taxonomy")
-public class DistinctTaxonomyCodeDTO {
+public class TaxonomyCodeDTO {
   @Id
   private String organizationType;
   private String organizationTypeDescription;
