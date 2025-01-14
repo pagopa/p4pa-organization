73cc7b1b5bf67730f08d9797f336db59bfd9bf94
oleksiybozhykntt <oleksiy.bozhyk@nttdata.com>
2025-01-14 15:19:09 +0100

    updated code

--- src/main/java/it/gov/pagopa/pu/organization/model/taxonomy/TaxonomyServiceTypeCodeDTO.java
+++ src/main/java/it/gov/pagopa/pu/organization/model/taxonomy/TaxonomyServiceTypeCodeDTO.java
@@ -6,9 +6,11 @@ import jakarta.persistence.IdClass;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "taxonomy")
 @IdClass(TaxonomyServiceTypeCodeDTO.class)
