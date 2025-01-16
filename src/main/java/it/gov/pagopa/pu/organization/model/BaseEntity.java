e7b639e0b89b15b49341ecf8a1ec6da60620f2db
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-01-16 15:13:09 +0100

    test

--- src/main/java/it/gov/pagopa/pu/organization/model/BaseEntity.java
+++ src/main/java/it/gov/pagopa/pu/organization/model/BaseEntity.java
@@ -1,6 +1,9 @@
 package it.gov.pagopa.pu.organization.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -13,8 +16,11 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 @Data
 @SuperBuilder(toBuilder = true)
@@ -25,11 +31,24 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity implements Serializable {
   @Column(updatable = false)
   @CreatedDate
-  @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+  @JsonSerialize(using = LocalDateTimeToOffsetDateTimeSerializer.class)
   private LocalDateTime creationDate;
   @LastModifiedDate
-  @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+  @JsonSerialize(using = LocalDateTimeToOffsetDateTimeSerializer.class)
   private LocalDateTime updateDate;
   @LastModifiedBy
   private String updateOperatorExternalId;
+
+  public static class LocalDateTimeToOffsetDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+      if (value != null) {
+        OffsetDateTime offsetDateTime = value.atOffset(ZoneOffset.UTC);
+        gen.writeString(offsetDateTime.toString());
+      }
+    }
+  }
 }
+
+
