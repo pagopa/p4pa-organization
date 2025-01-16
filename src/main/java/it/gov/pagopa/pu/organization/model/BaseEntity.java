4f5d1f0ef98d430a57f796957cb33781ce4ec3cc
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-01-16 13:06:43 +0100

    fix test

--- src/main/java/it/gov/pagopa/pu/organization/model/BaseEntity.java
+++ src/main/java/it/gov/pagopa/pu/organization/model/BaseEntity.java
@@ -25,10 +25,10 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity implements Serializable {
   @Column(updatable = false)
   @CreatedDate
-  @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-ddTHH:mm:ss.SSS")
+  @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss.SSS")
   private LocalDateTime creationDate;
   @LastModifiedDate
-  @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-ddTHH:mm:ss.SSS")
+  @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss.SSS")
   private LocalDateTime updateDate;
   @LastModifiedBy
   private String updateOperatorExternalId;
