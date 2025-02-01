f7e74f23fecee3c99d964783de275a8ce6a00fea
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-02-01 13:07:30 +0100

    fix: Required entity fields (#20)

--- src/main/java/it/gov/pagopa/pu/organization/model/OrgSilService.java
+++ src/main/java/it/gov/pagopa/pu/organization/model/OrgSilService.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.pu.organization.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,8 +20,11 @@ public class OrgSilService extends BaseEntity implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orgSilServiceGenerator")
   @SequenceGenerator(name = "orgSilServiceGenerator", sequenceName = "orgSilServiceSeq", allocationSize = 1)
+  @NotNull
   private Long orgSilServiceId;
+  @NotNull
   private Long organizationId;
+  @NotNull
   private String applicationName;
   private String notifyOutcomePushUrl;
   private boolean flagLegacy;
--- src/main/java/it/gov/pagopa/pu/organization/model/Taxonomy.java
+++ src/main/java/it/gov/pagopa/pu/organization/model/Taxonomy.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.pu.organization.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,18 +21,31 @@ public class Taxonomy extends BaseEntity implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "taxonomy_generator")
   @SequenceGenerator(name = "taxonomy_generator", sequenceName = "taxonomy_seq", allocationSize = 1)
+  @NotNull
   private Long taxonomyId;
+  @NotNull
   private String organizationType;
+  @NotNull
   private String organizationTypeDescription;
+  @NotNull
   private String macroAreaCode;
+  @NotNull
   private String macroAreaName;
+  @NotNull
   private String macroAreaDescription;
+  @NotNull
   private String serviceTypeCode;
+  @NotNull
   private String serviceType;
+  @NotNull
   private String serviceTypeDescription;
+  @NotNull
   private String collectionReason;
+  @NotNull
   private OffsetDateTime startDateValidity;
+  @NotNull
   private OffsetDateTime endDateOfValidity;
+  @NotNull
   private String taxonomyCode;
 
 }
