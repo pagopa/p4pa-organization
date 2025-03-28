c3f9d1e00519f7d108dc5105ededd6d5e4d1db93
pelliccm <matteo.pelliccione@nttdata.com>
2025-03-28 09:41:25 +0100

    [P4ADEV-2394] Organization findBy brokerId and optional orgName

--- src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationRepository.java
@@ -1,8 +1,11 @@
 package it.gov.pagopa.pu.organization.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import it.gov.pagopa.pu.organization.enums.OrganizationStatus;
 import it.gov.pagopa.pu.organization.model.Organization;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,19 +15,21 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
-import java.util.Optional;
-
 @RepositoryRestResource(path = "organizations")
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+public interface OrganizationRepository extends
+  JpaRepository<Organization, Long> {
 
   Optional<Organization> findByIpaCode(String ipaCode);
+
   Optional<Organization> findByOrgFiscalCode(String orgFiscalCode);
-  List<Organization> findByBrokerIdAndStatus(Long brokerId, OrganizationStatus status);
+
+  List<Organization> findByBrokerIdAndStatus(Long brokerId,
+    OrganizationStatus status);
 
   @Query("SELECT o FROM Organization o WHERE o.brokerId = :brokerId AND " +
-  "(:orgName is null OR o.orgName = :orgName)")
-  Page<Organization> findByBrokerIdAndOrgName(@Param("brokerId") Long brokerId, @Param("orgName") String orgName, Pageable pageable);
+    "(:orgName is null OR o.orgName ILIKE CONCAT('%', cast(:orgName as text), '%'))")
+  Page<Organization> findByBrokerIdAndOrgName(@Parameter(required = true) @Param("brokerId") Long brokerId,
+    @Param("orgName") String orgName, Pageable pageable);
 
   @Modifying
   @RestResource(exported = false)
