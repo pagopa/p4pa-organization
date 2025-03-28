4fe1c9b0c16dbc92b114db0caf8303fd5e8b9f72
pelliccm <107944358+pelliccm@users.noreply.github.com>
2025-03-28 10:32:40 +0100

    feat: P4ADEV-2394 Organization findBy brokerId and optional orgName (#38)

--- src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationRepository.java
@@ -1,23 +1,35 @@
 package it.gov.pagopa.pu.organization.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import it.gov.pagopa.pu.organization.enums.OrganizationStatus;
 import it.gov.pagopa.pu.organization.model.Organization;
 import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
-import java.util.Optional;
-
 @RepositoryRestResource(path = "organizations")
-public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+public interface OrganizationRepository extends
+  JpaRepository<Organization, Long> {
 
   Optional<Organization> findByIpaCode(String ipaCode);
+
   Optional<Organization> findByOrgFiscalCode(String orgFiscalCode);
-  List<Organization> findByBrokerIdAndStatus(Long brokerId, OrganizationStatus status);
+
+  List<Organization> findByBrokerIdAndStatus(Long brokerId,
+    OrganizationStatus status);
+
+  @Query("SELECT o FROM Organization o WHERE o.brokerId = :brokerId AND " +
+    "(:orgName is null OR o.orgName ILIKE CONCAT('%', cast(:orgName as text), '%'))")
+  Page<Organization> findByBrokerIdAndOrgName(@Parameter(required = true) @Param("brokerId") Long brokerId,
+    @Param("orgName") String orgName, Pageable pageable);
 
   @Modifying
   @Transactional
