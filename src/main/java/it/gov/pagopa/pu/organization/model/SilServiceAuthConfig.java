5993e11704ff58bbd16a19a6c3445b018f72335a
MScarsella <marco.scarsella@emeal.nttdata.com>
2025-06-23 15:43:15 +0200

    fix: P4ADEV-3150 added schema annotation in SilServiceAuthConfig.java (#64)
    Co-authored-by: mscarsel <marco.scarsella@nttdata.com>

--- src/main/java/it/gov/pagopa/pu/organization/model/SilServiceAuthConfig.java
+++ src/main/java/it/gov/pagopa/pu/organization/model/SilServiceAuthConfig.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.pu.organization.model;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.swagger.v3.oas.annotations.media.DiscriminatorMapping;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.io.Serializable;
 
@@ -11,5 +13,13 @@ import java.io.Serializable;
   @JsonSubTypes.Type(name = "legacyJwt", value = SilServiceLegacyJwtAuthConfig.class),
   @JsonSubTypes.Type(name = "legacyBasic", value = SilServiceLegacyBasicAuthConfig.class),
 })
+@Schema(
+        discriminatorProperty = "authConfig",
+        discriminatorMapping = {
+                @DiscriminatorMapping(value = "legacyJwt", schema = SilServiceLegacyJwtAuthConfig.class),
+                @DiscriminatorMapping(value = "legacyBasic", schema = SilServiceLegacyBasicAuthConfig.class)
+        },
+        oneOf = {SilServiceLegacyJwtAuthConfig.class, SilServiceLegacyBasicAuthConfig.class}
+)
 public interface SilServiceAuthConfig extends Serializable {
 }
