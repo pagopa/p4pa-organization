a37fc4ed7ce943b3311929164beaf7e6d2166046
ElisKina-dev <elis.kina@nttdata.com>
2025-07-16 17:12:05 +0200

    findOrgSilServicesByFilters API

---
+++ src/main/java/it/gov/pagopa/pu/organization/repository/view/OrgSilServiceViewRepository.java
@@ -0,0 +1,39 @@
+package it.gov.pagopa.pu.organization.repository.view;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import it.gov.pagopa.pu.organization.enums.OrgSilServiceType;
+import it.gov.pagopa.pu.organization.model.view.OrgSilServiceView;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "org-sil-services-view")
+public interface OrgSilServiceViewRepository extends Repository<OrgSilServiceView, Long> {
+
+  @Query("""
+    SELECT new OrgSilServiceView(
+    o.orgSilServiceId,
+    o.organizationId,
+    o.applicationName,
+    o.serviceUrl,
+    o.serviceType,
+    o.flagLegacy,
+    o.updateDate
+    )
+    FROM OrgSilService o
+    WHERE o.organizationId = :organizationId
+    AND (:applicationName IS NULL OR o.applicationName = :applicationName)
+    AND (:serviceType IS NULL OR o.serviceType = :serviceType)
+    AND (:flagLegacy IS NULL OR o.flagLegacy = :flagLegacy)
+    """)
+  Page<OrgSilServiceView> findOrgSilServicesByFilters(
+    @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("organizationId") Long organizationId,
+    @Param("applicationName") String applicationName,
+    @Param("serviceType") OrgSilServiceType serviceType,
+    @Param("flagLegacy") Boolean flagLegacy,
+    Pageable pageable);
+}
