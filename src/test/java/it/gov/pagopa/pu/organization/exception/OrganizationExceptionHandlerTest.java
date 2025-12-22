61fd5bca24513b1710642a963bcfd879cf8a2536
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-12-22 10:41:45 +0100

    fix: Fix CVE, upgrade SpringBoot 4 (#104)

--- src/test/java/it/gov/pagopa/pu/organization/exception/OrganizationExceptionHandlerTest.java
+++ src/test/java/it/gov/pagopa/pu/organization/exception/OrganizationExceptionHandlerTest.java
@@ -28,8 +28,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.MediaType;
@@ -50,7 +50,8 @@ import org.springframework.web.server.ServerErrorException;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 @ExtendWith({SpringExtension.class})
-@WebMvcTest(value = {OrganizationExceptionHandlerTest.TestController.class}, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+@WebMvcTest(value = {OrganizationExceptionHandlerTest.TestController.class})
+@AutoConfigureMockMvc(addFilters = false)
 @ContextConfiguration(classes = {
   OrganizationExceptionHandlerTest.TestController.class,
   OrganizationExceptionHandler.class,
@@ -180,7 +181,7 @@ class OrganizationExceptionHandlerTest {
     mockMvc.perform(MockMvcRequestBuilders.post("/NOTEXISTENTURL"))
       .andExpect(MockMvcResultMatchers.status().isNotFound())
       .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("ORGANIZATION_NOT_FOUND"))
-      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No static resource NOTEXISTENTURL."))
+      .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("No static resource NOTEXISTENTURL for request '/NOTEXISTENTURL'."))
       .andExpect(MockMvcResultMatchers.jsonPath("$.traceId").value(traceId));
   }
 
