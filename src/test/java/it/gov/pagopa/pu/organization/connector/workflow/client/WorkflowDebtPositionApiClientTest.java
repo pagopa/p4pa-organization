98128fa9bdb57a59983cace41715f8ef444bdfa2
michele.mazzini <michele.mazzini1996@gmail.com>
2026-04-10 17:05:08 +0200

    fix: add test

--- src/test/java/it/gov/pagopa/pu/organization/connector/workflow/client/WorkflowDebtPositionApiClientTest.java
+++ src/test/java/it/gov/pagopa/pu/organization/connector/workflow/client/WorkflowDebtPositionApiClientTest.java
@@ -1,8 +1,13 @@
 package it.gov.pagopa.pu.organization.connector.workflow.client;
 
 import it.gov.pagopa.pu.organization.connector.workflow.config.WorkflowApisHolder;
+import it.gov.pagopa.pu.workflowhub.controller.generated.DebtPositionApi;
+import it.gov.pagopa.pu.workflowhub.dto.generated.MassiveDebtPositionIbanUpdateRequestDTO;
+import it.gov.pagopa.pu.workflowhub.dto.generated.WorkflowCreatedDTO;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -12,6 +17,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class WorkflowDebtPositionApiClientTest {
   @Mock
   private WorkflowApisHolder workflowApisHolderMock;
+  @Mock
+  private DebtPositionApi debtPositionApiMock;
 
   private WorkflowDebtPositionApiClient workflowApiClient;
 
@@ -27,5 +34,20 @@ class WorkflowDebtPositionApiClientTest {
     );
   }
 
-  // TODO: add tests
+  @Test
+  void whenMassiveDpIbanUpdateThenInvokeWithAccessToken() {
+    String accessToken = "accessToken";
+    Long orgId = 1L;
+    MassiveDebtPositionIbanUpdateRequestDTO requestDTO = new MassiveDebtPositionIbanUpdateRequestDTO();
+    WorkflowCreatedDTO expectedResult = new WorkflowCreatedDTO();
+
+    Mockito.when(workflowApisHolderMock.getDebtPositionApi(accessToken))
+      .thenReturn(debtPositionApiMock);
+    Mockito.when(debtPositionApiMock.massiveDpIbanUpdate(orgId, requestDTO))
+      .thenReturn(expectedResult);
+
+    WorkflowCreatedDTO result = workflowApiClient.massiveDpIbanUpdate(orgId, requestDTO, accessToken);
+
+    Assertions.assertEquals(expectedResult, result);
+  }
 }
