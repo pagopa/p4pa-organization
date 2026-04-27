ae3b0548d64c462d7290a510e010a38572db7206
antocalo <antonio.calo@nttdata.com>
2026-04-27 16:49:39 +0200

    P4ADEV-4642-add-model-to-station-and-org-station

---
+++ src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationStationRepository.java
@@ -0,0 +1,7 @@
+package it.gov.pagopa.pu.organization.repository;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "organization-stations")
+public interface OrganizationStationRepository {
+}
---
+++ src/main/java/it/gov/pagopa/pu/organization/repository/StationRepository.java
@@ -0,0 +1,9 @@
+package it.gov.pagopa.pu.organization.repository;
+
+import it.gov.pagopa.pu.organization.model.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "stations")
+public interface StationRepository extends JpaRepository<Station, String> {
+}
