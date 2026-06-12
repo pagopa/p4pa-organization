1d8e032f721e29a3e3f8cd10cda22e125da5ac43
antocalo <antonio.calo@nttdata.com>
2026-06-12 11:47:33 +0200

    P4ADEV-4730-change-encryptAndSaveApiKey

--- src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationKeysRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/OrganizationKeysRepository.java
@@ -4,6 +4,6 @@ import it.gov.pagopa.pu.organization.model.OrganizationKeys;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "organization-keys")
+@RepositoryRestResource(path = "organization-keys", exported = false)
 public interface OrganizationKeysRepository extends JpaRepository<OrganizationKeys, String> {
 }
