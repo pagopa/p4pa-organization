2288df52da477b23da4ee15d74c0b17f690cb3d9
mscarsel <190512124+MScarsella@users.noreply.github.com>
2026-06-15 09:46:28 +0200

    [P4ADEV-4740] fixed pdndClient and pdndService paths

--- src/main/java/it/gov/pagopa/pu/organization/repository/PdndServiceRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/PdndServiceRepository.java
@@ -4,6 +4,6 @@ import it.gov.pagopa.pu.organization.model.PdndService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "pdnd-service")
+@RepositoryRestResource(path = "pdnd-services")
 public interface PdndServiceRepository extends JpaRepository<PdndService, String> {
 }
