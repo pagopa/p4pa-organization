52dee30c1ec1876f4e3e81e24f12b8807d02cd5e
mscarsel <190512124+MScarsella@users.noreply.github.com>
2026-06-17 16:14:55 +0200

    [P4ADEV-4781] fix log

--- src/main/java/it/gov/pagopa/pu/organization/controller/PdndClientController.java
+++ src/main/java/it/gov/pagopa/pu/organization/controller/PdndClientController.java
@@ -27,7 +27,7 @@ public class PdndClientController implements PdndClientApi {
 
   @Override
   public ResponseEntity<PdndClientDTO> getPdndClientByOrganizationIdAndPdndServiceType(Long organizationId, PdndServiceType pdndServiceType, String subUnitCode) {
-    log.info("Requested getPdndClientByOrganizationIdAndPdndServiceType with organizationId {}, pdndServiceType {} and subUnitCode {}", organizationId, pdndServiceType, subUnitCode);
+    log.info("Requested PDND client of organizationId {} and subUnitCode {} related to PDND service type {}", organizationId, subUnitCode, pdndServiceType);
     return ResponseEntity.ok(service.getPdndClientByOrganizationIdAndPdndServiceType(organizationId,pdndServiceType,subUnitCode));
   }
 }
