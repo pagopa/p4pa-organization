0ed42d3d8d02723944108214154f755c9cf91377
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2026-06-17 10:06:12 +0200

    fix: P4ADEV-4745 remove pp and tos from dto (#172)

--- src/main/java/it/gov/pagopa/pu/organization/dto/PersonalisationFe.java
+++ src/main/java/it/gov/pagopa/pu/organization/dto/PersonalisationFe.java
@@ -9,9 +9,7 @@ public class PersonalisationFe implements Serializable {
   private String headerAssistanceUrl;
   private String logoFooterImg;
   private String footerDescText;
-  private String footerPrivacyInfoUrl;
   private String footerGDPRUrl;
-  private String footerTermsCondUrl;
   private String footerAccessibilityUrl;
 
 }
