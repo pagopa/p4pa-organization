d90184e66b870c383b0bdf8f6614254b921a881c
mscarsel <190512124+MScarsella@users.noreply.github.com>
2026-06-18 12:49:16 +0200

    fix PdndServiceRepository.findByClientId

--- src/main/java/it/gov/pagopa/pu/organization/repository/PdndServiceRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/PdndServiceRepository.java
@@ -1,10 +1,11 @@
 package it.gov.pagopa.pu.organization.repository;
 
+import it.gov.pagopa.pu.organization.enums.PdndServiceType;
 import it.gov.pagopa.pu.organization.model.PdndService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "pdnd-services")
 public interface PdndServiceRepository extends JpaRepository<PdndService, String> {
-  PdndService findByClientId(String clientId);
+  PdndService findByClientIdAndServiceType(String clientId, PdndServiceType serviceType);
 }
