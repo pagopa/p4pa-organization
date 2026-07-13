0e8dc8c40330935a187ef92818a83f46ff5199cf
MScarsella <marco.scarsella@pagopa.it>
2026-07-13 16:13:22 +0200

    feat: P4ADEV-4891 add query OrgSubUnitOperatorsRepository.findByOrganizationIdAndSubUnitCodeAndOperatorExternalUserId (#200)

--- src/main/java/it/gov/pagopa/pu/organization/repository/OrgSubUnitOperatorsRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/OrgSubUnitOperatorsRepository.java
@@ -4,6 +4,9 @@ import it.gov.pagopa.pu.organization.model.OrgSubUnitOperators;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource(path = "org-sub-unit-operators")
 public interface OrgSubUnitOperatorsRepository extends JpaRepository<OrgSubUnitOperators,Long> {
+  Optional<OrgSubUnitOperators> findByOrganizationIdAndSubUnitCodeAndOperatorExternalUserId(Long organizationId, String subUnitCode, String operatorExternalUserId);
 }
