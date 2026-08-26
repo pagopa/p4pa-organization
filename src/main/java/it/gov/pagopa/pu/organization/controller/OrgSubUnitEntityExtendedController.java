8f96874b50137ee658a37682a7e1b61d7b170cff
michele.mazzini <michele.mazzini1996@gmail.com>
2026-08-26 15:14:43 +0200

    fix: add log.info

--- src/main/java/it/gov/pagopa/pu/organization/controller/OrgSubUnitEntityExtendedController.java
+++ src/main/java/it/gov/pagopa/pu/organization/controller/OrgSubUnitEntityExtendedController.java
@@ -3,11 +3,13 @@ package it.gov.pagopa.pu.organization.controller;
 import it.gov.pagopa.pu.organization.controller.generated.OrgSubUnitEntityExtendedControllerApi;
 import it.gov.pagopa.pu.organization.enums.OrgSubUnitStatus;
 import it.gov.pagopa.pu.organization.repository.OrgSubUnitRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Controller to host spring-data-rest directly not supported methods */
 @RestController
+@Slf4j
 public class OrgSubUnitEntityExtendedController implements OrgSubUnitEntityExtendedControllerApi {
   private final OrgSubUnitRepository repository;
 
@@ -17,6 +19,7 @@ public class OrgSubUnitEntityExtendedController implements OrgSubUnitEntityExten
 
   @Override
   public ResponseEntity<Void> updateStatus(Long organizationId, String subUnitCode, OrgSubUnitStatus status) {
+    log.info("Updating status to {} for OrgSubUnit [organizationId={}, subUnitCode={}]", status, organizationId, subUnitCode);
     repository.updateStatus(organizationId, subUnitCode, status);
     return ResponseEntity.ok().build();
   }
