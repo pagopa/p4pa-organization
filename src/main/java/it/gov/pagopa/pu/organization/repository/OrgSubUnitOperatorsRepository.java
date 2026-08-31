581df089a8069397a47a8a26aa0403750e678002
Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>
2026-08-31 15:13:23 +0200

    fix: P4ADEV-4935 add query findByOrganizationIdAndSubUnitCode (#218)

--- src/main/java/it/gov/pagopa/pu/organization/repository/OrgSubUnitOperatorsRepository.java
+++ src/main/java/it/gov/pagopa/pu/organization/repository/OrgSubUnitOperatorsRepository.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.pu.organization.repository;
 
 import it.gov.pagopa.pu.organization.model.OrgSubUnitOperators;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -9,4 +11,6 @@ import java.util.Optional;
 @RepositoryRestResource(path = "org-sub-unit-operators")
 public interface OrgSubUnitOperatorsRepository extends JpaRepository<OrgSubUnitOperators,Long> {
   Optional<OrgSubUnitOperators> findByOrganizationIdAndSubUnitCodeAndOperatorExternalUserId(Long organizationId, String subUnitCode, String operatorExternalUserId);
+
+  Page<OrgSubUnitOperators> findByOrganizationIdAndSubUnitCode(Long organizationId, String subUnitCode, Pageable pageable);
 }
