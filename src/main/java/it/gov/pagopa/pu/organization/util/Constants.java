21ca901d7fe665bbcfaa2c1c99e2d95b32308413
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2026-09-04 17:39:42 +0200

    fix: Uniform update date time to BaseEntityListener (#225)

--- src/main/java/it/gov/pagopa/pu/organization/util/Constants.java
+++ src/main/java/it/gov/pagopa/pu/organization/util/Constants.java
@@ -11,8 +11,8 @@ public class Constants {
   public static final TimeZone DEFAULT_TIMEZONE = TimeZone.getTimeZone(ZONEID);
   public static final String DEFAULT_IUV_SYSTEM_ID = "00";
 
-  public static final String UPDATE_AUDIT_FIELDS_SPEL = "updateDate = CURRENT_TIMESTAMP, "
-    + "updateOperatorExternalId = :#{T(it.gov.pagopa.pu.organization.util.SecurityUtils).getCurrentUserExternalId()}, "
-    + "updateTraceId = :#{T(it.gov.pagopa.pu.organization.util.Utilities).getTraceId()}";
+  public static final String UPDATE_AUDIT_FIELDS_SPEL = "updateDate = :#{T(java.time.LocalDateTime).now()}, " +
+    "updateOperatorExternalId = :#{T(it.gov.pagopa.pu.organization.util.SecurityUtils).getCurrentUserExternalId()}, " +
+    "updateTraceId = :#{T(it.gov.pagopa.pu.organization.util.Utilities).getTraceId()}";
 }
 
